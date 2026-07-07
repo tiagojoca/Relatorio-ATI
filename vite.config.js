55a7533111f5860f05d7d62d0e64d9f9b78b59ea
tiago <tiago@CBM-ATI.com>
2026-07-06 22:06:05 -0300

    chore: adiciona firebase e vitest

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,4 +6,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Relatorio-ATI/',
   plugins: [react()],
+  test: {
+    environment: 'node',
+    include: ['src/**/*.test.{js,jsx}'],
+  },
 })
